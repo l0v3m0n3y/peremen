94c4661a1edfd4b68373ada4098cc287ea0180e1
in my mind... <94906343+l0v3m0n3y@users.noreply.github.com>
2026-08-25 18:48:51 +0300

    Add HTTPMethod enum for request methods

--- src/peremen.swift
+++ src/peremen.swift
@@ -20,6 +20,14 @@ extension URLSession {
     }
 }
 
+public enum HTTPMethod: String {
+    case get = "GET"
+    case post = "POST"
+    case put = "PUT"
+    case delete = "DELETE"
+    case patch = "PATCH"
+}
+
 public class Peremen {
     private let api = "https://peremen.space/api/v2"
     private var headers: [String: String]
